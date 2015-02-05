0b450142d07c976a279cf349e67eba8b81e30cbe
Tom Faulhaber <git_com@infolace.com>
2015-02-05 11:10:53 -0800

    Bump version to 0.2.1 and remove explicit dependency on Clojure.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject com.infolace/excel-templates "0.2.0"
+(defproject com.infolace/excel-templates "0.2.1"
   :description "Build Excel files by combining a template with plain old data"
   :url "https://github.com/tomfaulhaber/excel-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.apache.poi/poi-ooxml "3.10-FINAL"]
-                 [org.clojure/clojure      "1.6.0"]])
+  :dependencies [[org.apache.poi/poi-ooxml "3.10-FINAL"]])
