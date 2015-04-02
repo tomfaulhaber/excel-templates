4082e3eff0bf23946ad6b6a110f341680bff47d2
Tom Faulhaber <git_com@infolace.com>
2015-04-02 10:33:40 -0700

    Support for formulas and charts that depend on data that's expanding in the template (first version).

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject com.infolace/excel-templates "0.2.1"
+(defproject com.infolace/excel-templates "0.3.0-SNAPSHOT"
   :description "Build Excel files by combining a template with plain old data"
   :url "https://github.com/tomfaulhaber/excel-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure      "1.6.0"]
-                 [org.apache.poi/poi-ooxml "3.10-FINAL"]])
+  :dependencies [[org.apache.poi/poi-ooxml "3.10-FINAL"]
+                 [org.apache.poi/ooxml-schemas "1.1"]
+                 [org.clojure/data.zip "0.1.1"]])
