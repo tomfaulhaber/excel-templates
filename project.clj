3ee41be9751939f63b62fbfa1a329732d9e3d501
Tom Faulhaber <git_com@infolace.com>
2015-10-07 20:47:27 -0700

    Remove specific dependencies on the version of Clojure

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject com.infolace/excel-templates "0.3.0"
+(defproject com.infolace/excel-templates "0.3.1-SNAPSHOT"
   :description "Build Excel files by combining a template with plain old data"
   :url "https://github.com/tomfaulhaber/excel-templates"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.poi/poi-ooxml "3.10-FINAL"]
                  [org.apache.poi/ooxml-schemas "1.1"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [joda-time "2.7"]])
+                 [org.clojure/data.zip "0.1.1" :exclusions [[org.clojure/clojure]]]
+                 [joda-time "2.7"]]
+  :profiles {:repl {:dependencies [[org.clojure/clojure "1.7.0"]]}})
